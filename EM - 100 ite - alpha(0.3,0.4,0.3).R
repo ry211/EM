680baaee356b7df893eb74cb6f6613a5fb9fc21c
ry211 <98042522+ry211@users.noreply.github.com>
2022-09-13 18:31:54 -0400

    Add files via upload

--- EM - 100 ite - alpha(0.3,0.4,0.3).R
+++ EM - 100 ite - alpha(0.3,0.4,0.3).R
@@ -14,15 +14,19 @@ epsilon=0.0001
 k=3
 maxtimes_iterations=10000
 alp = list()
+order = list()
 
+#
 
+#
 
 ################### EM-function #####################
 GMM = function(dat,w_ik0,alpha,mu_s,sigma_s,epsilon,maxtimes_iterations){
+  w_ik_old = w_ik0
+  k=ncol(w_ik0)
+  true_mu = matrix(unlist(meann),3,3,byrow = T)
   
   for(i in 1:maxtimes_iterations){ 
-    
-    k=ncol(w_ik0)
     
     #E-step
     #w_ik
@@ -36,6 +40,12 @@ GMM = function(dat,w_ik0,alpha,mu_s,sigma_s,epsilon,maxtimes_iterations){
     N_k = colSums(w_ik)
     alpha = N_k/nrow(dat)
     #alp[[i]] = alpha
+    order(alpha)
+    dat = dat[,order(alpha)]
+    N_k = N_k[order(alpha)]
+    w_ik = w_ik[,order(alpha)]
+    w_ik_old = w_ik_old[,order(alpha)]
+    true_mu = true_mu[,order(alpha)]
     
     for(j in 1:k){
       mu_s[[j]] = (t(as.matrix(w_ik[, j]))%*%dat) / N_k[j]
@@ -56,7 +66,7 @@ GMM = function(dat,w_ik0,alpha,mu_s,sigma_s,epsilon,maxtimes_iterations){
     
   }
   
-  list(mu=mu_s, COV=sigma_s, weight=w_ik,alpha=alpha,ite_time=i) 
+  list(mu=mu_s, COV=sigma_s, weight=w_ik,alpha=alpha,ite_time=i,true_mu=true_mu) 
 }
 
 
@@ -73,7 +83,7 @@ for (ite in 1:100) {
   moni3 = mvrnorm(300,mean3,sd3)
   dat  = rbind(moni1,moni2,moni3)  #means true value of alpha is c(0.3,0.4,0.3)
   w_ik0 = matrix(0, ncol = k, nrow = nrow(dat)) 
-  w_ik_old=w_ik0
+  #w_ik_old=w_ik0
   
   # initial value
   alpha=rep(1/k,k)
@@ -95,15 +105,16 @@ ye_mean1 = list()
 ye_alpha1 = list()
 
 for (ite in 1:100) {
-  ye_mean1[[ite]]=matrix(unlist(ye1[[ite]]$mu),3,3,byrow = T) - matrix(unlist(meann),3,3,byrow = T)
+  ye_mean1[[ite]]=matrix(unlist(ye1[[ite]]$mu),3,3,byrow = T) - ye1[[ite]]$true_mu
+    #matrix(unlist(meann),3,3,byrow = T)
   #ye_alpha1[[ite]]=matrix(unlist(ye1[[ite]]$alpha),length(unlist(ye1[[ite]]$alpha))/2,2,byrow = T) - matrix(c(0.7,0.3),length(unlist(ye1[[ite]]$alpha))/2,2,byrow = T)
-  ye_alpha1[[ite]]=unlist(ye1[[ite]]$alpha)-c(0.3,0.4,0.3)}  
+  ye_alpha1[[ite]]=unlist(ye1[[ite]]$alpha)-c(0.4,0.3,0.3)}  
 
-avg_bias = sum(abs(unlist(ye_mean1)))/length(unlist(ye_mean1)) #1.157782
-avg_bias1 = sum(unlist(ye_mean1))/length(unlist(ye_mean1)) #-0.02348967
-MSE=sum((unlist(ye_mean1))^2)/length(unlist(ye_mean1)) #2.026208
-avg_alpha = sum(abs(unlist(ye_alpha1)))/length(unlist(ye_alpha1)) #0.07794756
-avg_alpha1 = sum(unlist(ye_alpha1))/length(unlist(ye_alpha1)) #1.850372e-19
+avg_bias = sum(abs(unlist(ye_mean1)))/length(unlist(ye_mean1)) #1.580695
+avg_bias1 = sum(unlist(ye_mean1))/length(unlist(ye_mean1)) #-0.005101904
+MSE=sum((unlist(ye_mean1))^2)/length(unlist(ye_mean1)) #3.482015
+avg_alpha = sum(abs(unlist(ye_alpha1)))/length(unlist(ye_alpha1)) #0.08570575
+avg_alpha1 = sum(unlist(ye_alpha1))/length(unlist(ye_alpha1)) #1.110223e-18
 
 
 
